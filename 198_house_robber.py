b8fcb9a1f4a3548ed3c384c7a29c7cb6305c6618
Bartek <bartekg42@gmail.com>
2025-08-15 19:13:00 +0200

    Leetcode 198

---
+++ 198_house_robber.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+def rob(nums: List[int]) -> int:
+    if not nums:
+        return 0
+    elif len(nums) <= 2:
+        return max(nums)
+    dp = [0] * len(nums)
+    for index in range(0, len(nums)):
+        # Might be done better cause you access index -2 and index -1 in the first two iterations
+        dp[index] = (max(dp[index - 2] + nums[index], dp[index - 1]))
+    return max(dp)
+
+
+if __name__ == "__main__":
+    nums1 = [1, 2, 3, 1]
+    nums2 = [2, 7, 9, 3, 1]
+    assert (rob(nums1)) == 4
+    assert (rob(nums2)) == 12
+    assert (rob([])) == 0
