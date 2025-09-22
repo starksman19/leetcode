998849ce192cb3f27d9c5be6a3975de4460c535b
Bartek <bartekg42@gmail.com>
2025-09-22 22:46:00 +0200

    ruff

--- 120_triangle.py
+++ 120_triangle.py
@@ -11,18 +11,26 @@ from typing import List
 
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        mem = [[float("inf") for _ in range(len(triangle[row]))] for row in range(len(triangle))]
+        mem = [
+            [float("inf") for _ in range(len(triangle[row]))]
+            for row in range(len(triangle))
+        ]
         mem[0][0] = triangle[0][0]
         for i in range(1, len(triangle)):
             for j in range(len(triangle[i])):
                 if j == 0:
-                    mem[i][j] = triangle[i][j] + mem[i-1][j]
+                    mem[i][j] = triangle[i][j] + mem[i - 1][j]
                 elif j == len(triangle[i]) - 1:
-                    mem[i][j] = triangle[i][j] + mem[i-1][j-1]
+                    mem[i][j] = triangle[i][j] + mem[i - 1][j - 1]
                 else:
-                    mem[i][j] = min(triangle[i][j] + mem[i-1][j], triangle[i][j] + mem[i-1][j-1])
+                    mem[i][j] = min(
+                        triangle[i][j] + mem[i - 1][j],
+                        triangle[i][j] + mem[i - 1][j - 1],
+                    )
         return int(min(mem[-1]))
-triangle1 = [[2],[3,4],[6,5,7],[4,1,8,3]]
+
+
+triangle1 = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]
 triangle2 = [[-10]]
 
 
