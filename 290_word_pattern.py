28e47ebcc2193c15e9c84bc557afc679f22de946
Bartek <bartekg42@gmail.com>
2025-09-23 21:56:06 +0200

    290 word pattern recommit, didnt add in prev commit

--- 290_word_pattern.py
+++ 290_word_pattern.py
@@ -8,6 +8,31 @@
 
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        pass
+        pattern_map = {}
+        word_list = s.split(" ")
+        if len(pattern) != len(word_list):
+            return False
+        for i in range(len(pattern)):
+            if pattern[i] not in pattern_map:
+                if word_list[i] not in pattern_map.values():
+                    pattern_map[pattern[i]] = word_list[i]
+                else:
+                    return False
+            else:
+                if not pattern_map[pattern[i]] == word_list[i]:
+                    return False
+        return True
 
 
+
+pattern1  = "abba"
+t1 = "dog cat cat dog"
+
+pattern2 = "abba"
+t2 = "dog cat cat fish"
+
+print(Solution().wordPattern(pattern1 , t1))
+assert Solution().wordPattern(pattern1 , t1) == True
+
+print(Solution().wordPattern(pattern2, t2))
+assert Solution().wordPattern(pattern2, t2) == False
