5d344df122e5a4dea6fc9e714d9998561de11ca1
Bartek <bartekg42@gmail.com>
2025-09-23 22:06:48 +0200

    205 isomporphic strings

--- 205_isomorphic_strings.py
+++ 205_isomorphic_strings.py
@@ -8,16 +8,28 @@
 
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        pass
+        if len(s) != len(t):
+            return False
+        letter_map = {}
+        for i in range(len(s)):
+            if s[i] not in letter_map:
+                if t[i] not in letter_map.values():
+                    letter_map[s[i]] = t[i]
+                else:
+                    return False
+            else:
+                if letter_map[s[i]] != t[i]:
+                    return False
+        return True
 
 s1 = "egg"
 t1 = "add"
 
-s2 = "paper"
-t2 = "title"
+s2 = "foo"
+t2 = "bar"
 
 print(Solution().isIsomorphic(s1, t1))
 assert Solution().isIsomorphic(s1, t1) == True
 
 print(Solution().isIsomorphic(s2, t2))
-assert Solution().isIsomorphic(s2, t2) == True
+assert Solution().isIsomorphic(s2, t2) == False
