c0e4ade392ac9494aed1e8df667dabb7fc6086ba
Bartek <bartekg42@gmail.com>
2025-09-26 01:48:47 +0200

    139 word break

--- 139_word_break.py
+++ 139_word_break.py
@@ -8,7 +8,15 @@ from typing import List
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        pass
+        word_set = set(wordDict)
+        dp = [False] * (len(s) + 1) # is word of len i in dict. In loop check if shorter setups are avaliable, all lenghts previously
+        dp[0] = True
+        for i in range(1, len(s)+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True
+                    break
+        return dp[-1]
 
 
 s1 = "leetcode"
