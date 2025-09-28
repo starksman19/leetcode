46a6e23a1a6e76d249acdb0264808e961366974d
Bartek <bartekg42@gmail.com>
2025-09-28 17:10:21 +0200

    adding warmup

--- warmup.py
+++ warmup.py
@@ -358,3 +358,32 @@ def coin_change_warmup(coins: List[int], amount: int):
 
 
 assert coin_change_warmup([1, 2, 5], 11) == 3
+
+
+# ---------------------------
+# 13. Backtracking - combinationSum
+
+def comination_sum(candidates: List[int], target: int) -> List[List[int]]:
+    """
+    # Given an array of distinct integers candidates and a target integer target,
+    # return a list of all unique combinations of candidates where the chosen numbers sum to target.
+    # You may return the combinations in any order.
+    #
+    # The same number may be chosen from candidates an unlimited number of times.
+    # Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+    #
+    # The test cases are generated such that the number of unique combinations
+    # that sum up to target is less than 150 combinations for the given input.
+    """
+    pass
+
+candidates1 = [2, 3, 5]
+target1 = 8
+candidates2 = [2]
+target2 = 1
+assert comination_sum(candidates1, target1) == [
+    [2, 2, 2, 2],
+    [2, 3, 3],
+    [3, 5],
+]
+assert comination_sum(candidates2, target2) == []
