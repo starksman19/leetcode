99f630f810092724dd52202abe1980f9bef69e4e
Bartek <bartekg42@gmail.com>
2025-09-30 00:49:48 +0200

    22 generate parenthesis

--- 150_interview/15_backtracking/22_generate_parentheses.py
+++ 150_interview/15_backtracking/22_generate_parentheses.py
@@ -5,8 +5,21 @@ from typing import List
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        pass
-
+        ret = []
+        def backtrack(path: List[str], open_count: int, close_count: int):
+            if len(path) == 2*n:
+                ret.append("".join(path[:]))
+                return
+            if open_count < n:
+                path.append("(")
+                backtrack(path, open_count + 1, close_count)
+                path.pop()
+            if close_count < open_count:
+                path.append(")")
+                backtrack(path, open_count, close_count + 1)
+                path.pop()
+        backtrack([], 0, 0)
+        return ret
 
 n1 = 3
 n2 = 1
