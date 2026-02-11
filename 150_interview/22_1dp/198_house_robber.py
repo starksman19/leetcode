4401d098d0feea1c37c07b30ec1bbcdafb70d138
Bartek <bartekg42@gmail.com>
2026-02-12 00:44:46 +0100

    house robber quick reminder

--- 150_interview/22_1dp/198_house_robber.py
+++ 150_interview/22_1dp/198_house_robber.py
@@ -1,16 +1,31 @@
 from typing import List
 
+# You are a professional robber planning to rob houses along a street.
+# Each house has a certain amount of money stashed,
+# the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected
+# and it will automatically contact the police if two adjacent houses were broken into on the same night.
+#
+# Given an integer array nums representing the amount of money of each house
+# return the maximum amount of money you can rob tonight without alerting the police.
+
 
 def rob(nums: List[int]) -> int:
     if not nums:
         return 0
-    elif len(nums) <= 2:
+    elif len(nums) == 1:
+        return nums[0]
+    elif len(nums) == 2:
         return max(nums)
-    dp = [0] * len(nums)
-    for index in range(0, len(nums)):
-        # Might be done better cause you access index -2 and index -1 in the first two iterations
-        dp[index] = max(dp[index - 2] + nums[index], dp[index - 1])
-    return max(dp)
+
+    first, second = (
+        max(nums[0], nums[1]),
+        nums[0],
+    )
+    for i in range(2, len(nums)):
+        temp = first
+        first = max(second + nums[i], first)
+        second = temp
+    return first
 
 
 if __name__ == "__main__":
@@ -19,3 +34,6 @@ if __name__ == "__main__":
     assert (rob(nums1)) == 4
     assert (rob(nums2)) == 12
     assert (rob([])) == 0
+    assert (rob([1])) == 1
+    assert (rob([1, 2])) == 2
+    assert (rob([2, 1, 1, 2])) == 4
