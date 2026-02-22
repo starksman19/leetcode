f07d1d46e8bf20955b25854c2cf8f17fc0ff0216
Bartek <bartekg42@gmail.com>
2026-02-22 21:10:58 +0100

    122 best time to buy stock 2

--- 150_interview/1_array_string/122_best_time_to_buy_stock_2.py
+++ 150_interview/1_array_string/122_best_time_to_buy_stock_2.py
@@ -14,20 +14,16 @@ class Solution:
         if not prices:
             return 0
 
-        max_price = float("-inf")
-        min_price = float("inf")
-        biggest_diff_this_far = []
+        profit = 0
 
-        for profit in prices:
-            max_price = profit if profit > max_price else max_price
-            min_price = profit if profit < min_price else min_price
-            biggest_diff_this_far.append(max_price - min_price)
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
 
-        return max(biggest_diff_this_far)
+        return profit
 
 
 prices = [7, 1, 5, 3, 6, 4]
-
 print(Solution().maxProfit(prices))
 assert Solution().maxProfit(prices) == 7
 
