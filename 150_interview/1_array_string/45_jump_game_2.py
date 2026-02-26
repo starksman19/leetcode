77da2ee6138a8e951a185ad46d6e641c5a2cfcaf
Bartek <bartekg42@gmail.com>
2026-02-26 02:16:25 +0100

    45 jump game 2

--- 150_interview/1_array_string/45_jump_game_2.py
+++ 150_interview/1_array_string/45_jump_game_2.py
@@ -11,11 +11,26 @@ from typing import List
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        pass
+        n = len(nums)
+        if n <= 1:
+            return 0
+
+        jumps = 0
+        current_end = 0
+        farthest = 0
+
+        for i in range(n - 1):
+            farthest = max(farthest, i + nums[i])
+            if i == current_end:
+                current_end = farthest
+                jumps += 1
+
+        return jumps
 
 
 nums1 = [2, 3, 1, 1, 4]
 nums2 = [2, 3, 0, 1, 4]
+nums3 = [0]
 
 
 print(Solution().jump(nums1))
@@ -23,3 +38,6 @@ assert Solution().jump(nums1) == 2
 
 print(Solution().jump(nums2))
 assert Solution().jump(nums2) == 2
+
+print(Solution().jump(nums3))
+assert Solution().jump(nums3) == 0
