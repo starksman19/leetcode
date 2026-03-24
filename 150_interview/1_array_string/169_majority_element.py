e37fa2c23123a58136a4a0c73d0b848d28b353d3
Bartek <bartekg42@gmail.com>
2026-03-24 21:02:10 +0100

    169 soluton

--- 150_interview/1_array_string/169_majority_element.py
+++ 150_interview/1_array_string/169_majority_element.py
@@ -8,7 +8,33 @@ from typing import List
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        pass
+        if len(nums) == 1:
+            return nums[0]
+        val_map = {}
+        for i in range(len(nums)):
+            if nums[i] not in val_map:
+                val_map[nums[i]] = 1
+            else:
+                val_map[nums[i]] += 1
+        for key, val in val_map.items():
+            if val > len(nums) / 2:
+                return key
+        return None
+
+    def majorityElement_but_better(self, nums: List[int]) -> int:
+        candidate = None
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
 
 
 nums1 = [3, 2, 3]
