8c0d547ade49175977345135167ee07803814bde
Bartek <bartekg42@gmail.com>
2026-05-13 03:12:09 +0200

    q7 idk from where

--- google_interview/q7.py
+++ google_interview/q7.py
@@ -1,11 +1,41 @@
 # Given an integer array nums and an integer k, return an array of the maximum values of each sliding window of size k.
 #
 # A sliding window moves from left to right by one position at a time.
+from collections import deque
 
 
 class Solution:
     def maxSlidingWindow(self, nums, k):
-        pass
+        if not nums:
+            return []
+
+        que = deque()
+        ret = []
+        for i in range(len(nums)):
+            while que and que[0] <= i - k:
+                que.popleft()
+
+            while que and nums[que[-1]] < nums[i]:
+                que.pop()
+
+            que.append(i)
+
+            if i >= k - 1:
+                ret.append(nums[que[0]])
+
+        return ret
+
+    def maxSlidingWindow2(self, nums, k):
+        if not nums:
+            return []
+
+        left, right = 0, k
+        ret = []
+        while right < len(nums) + 1:
+            ret.append(max(nums[left:right]))
+            left += 1
+            right += 1
+        return ret
 
 
 s = Solution()
@@ -30,6 +60,3 @@ assert s.maxSlidingWindow([5, 4, 3, 2, 1], 2) == [5, 4, 3, 2]
 
 # rosnąca tablica
 assert s.maxSlidingWindow([1, 2, 3, 4, 5], 2) == [2, 3, 4, 5]
-
-
-left, right = []
