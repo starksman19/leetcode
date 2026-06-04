dddf28f0807592320d672755be9d9fbd8819c12c
Bartek <bartekg42@gmail.com>
2026-06-04 20:02:43 +0200

    04.06 two tasks

--- google_interview/onsite_learning/04_06_contains_duplicate.py
+++ google_interview/onsite_learning/04_06_contains_duplicate.py
@@ -1,11 +1,20 @@
 # LeetCode: https://leetcode.com/problems/contains-duplicate/
 # Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        pass
+        visited = defaultdict(int)  # -> można też na secie to zrobić
+        for item in nums:
+            visited[item] += 1
+            if visited[item] > 1:
+                return True
+        return False
+
+    def containsDuplicate_most_pythonic(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
 
 
 nums = [1, 2, 3, 1]
--- google_interview/onsite_learning/04_06_valid_palindrome.py
+++ google_interview/onsite_learning/04_06_valid_palindrome.py
@@ -7,8 +7,24 @@
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        pass
+        left, right = 0, len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
 
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
+
+
+s = " "
+
+print(Solution().isPalindrome(s))
+assert Solution().isPalindrome(s) == True
 
 s = "A man, a plan, a canal: Panama"
 
