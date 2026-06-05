74dd78eec953ea1cc39e80a34bacd5ce029e5a36
Bartek <bartekg42@gmail.com>
2026-06-05 18:00:09 +0200

    05.06 longest substring wihtout repeating characters

--- google_interview/onsite_learning/05_06_longest_substring_without_repeating_characters.py
+++ google_interview/onsite_learning/05_06_longest_substring_without_repeating_characters.py
@@ -1,11 +1,39 @@
 # LeetCode: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 # Given a string s, return the length of the longest substring that contains no repeated characters.
+from collections import deque
 
 
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        pass
+    def lengthOfLongestSubstring_worse(self, s: str) -> int:
+        string = deque()
+        ret = 0
+        for letter in s:
+            if letter in string:
+                while letter in string:
+                    string.popleft()
+            string.append(letter)
+            ret = max(ret, len(string))
 
+        return ret
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        start, stop, ret = 0, 0, 0
+        letters = set()
+        while stop < len(s):
+            if s[stop] in letters:
+                letters.remove(s[start])
+                start += 1
+            else:
+                letters.add(s[stop])
+                stop += 1
+                ret = max(ret, stop - start)
+        return ret
+
+
+s = "aab"
+
+print(Solution().lengthOfLongestSubstring(s))
+assert Solution().lengthOfLongestSubstring(s) == 2
 
 s = "abcabcbb"
 
