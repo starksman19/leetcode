847df198958a72368d65a63a4085baebdb95d1a0
Bartek <bartekg42@gmail.com>
2026-06-14 22:28:40 +0200

    14.06 tasks

--- google_interview/onsite_learning/14_06_clone_graph.py
+++ google_interview/onsite_learning/14_06_clone_graph.py
@@ -1,5 +1,21 @@
 # LeetCode: https://leetcode.com/problems/clone-graph/
-# Given a reference to a node in a connected undirected graph, return a deep copy of the graph.
+# Given a reference of a node in a connected undirected graph.
+#
+# Return a deep copy (clone) of the graph.
+#
+# Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+#
+# class Node {
+#     public int val;
+#     public List<Node> neighbors;
+# }
+#
+# For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2,
+# and so on. The graph is represented in the test case using an adjacency list.
+#
+# An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+#
+# The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
 
 from collections import deque
 from typing import Dict, List, Optional
@@ -12,8 +28,53 @@ class Node:
 
 
 class Solution:
+    def cloneGraph_longer(self, node: Optional[Node]) -> Optional[Node]:
+        if not node:
+            return
+        visited = set()
+        new_root = Node(val=node.val)
+        node_map = {node: new_root}
+        visited.add(node)
+        que = [child for child in node.neighbors]
+
+        while que:
+            current = que.pop()
+            if current in visited:
+                continue
+            visited.add(current)
+            new_node = Node(val=current.val)
+            node_map[current] = new_node
+            for child in current.neighbors:
+                que.append(child)
+
+        que = [node]
+        visited.clear()
+        while que:
+            current = que.pop()
+            mapped_new = node_map[current]
+            if current in visited:
+                continue
+            visited.add(current)
+            for child in current.neighbors:
+                que.append(child)
+                mapped_new.neighbors.append(node_map[child])
+
+        return new_root
+
     def cloneGraph(self, node: Optional[Node]) -> Optional[Node]:
-        pass
+        if not node:
+            return
+        node_map = {node: Node(val=node.val)}
+        que = [node]
+
+        while que:
+            curr = que.pop()
+            for neighbour in curr.neighbors:
+                if neighbour not in node_map:
+                    node_map[neighbour] = Node(val=neighbour.val)
+                    que.append(neighbour)
+                node_map[curr].neighbors.append(node_map[neighbour])
+        return node_map[node]
 
 
 def build_graph(adj_list: List[List[int]]) -> Optional[Node]:
--- google_interview/onsite_learning/14_06_network_delay_time.py
+++ google_interview/onsite_learning/14_06_network_delay_time.py
@@ -1,13 +1,80 @@
 # LeetCode: https://leetcode.com/problems/network-delay-time/
-# Given directed travel times between nodes, return how long it takes for all nodes to receive a signal from k.
-# Return -1 if some node cannot be reached.
-
+# You are given a network of n nodes, labeled from 1 to n. You are also given times, a list of travel times as directed edges times[i] = (ui, vi, wi),
+# where ui is the source node, vi is the target node, and wi is the time it takes for a signal to travel from source to target.
+#
+# We will send a signal from a given node k. Return the minimum time it takes for all the n nodes to receive the signal.
+# If it is impossible for all the n nodes to receive the signal, return -1.
+import heapq
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
+    def networkDelayTime_mine(self, times: List[List[int]], n: int, k: int) -> int:
+        routes_map = {}
+        times_map = {}
+        for source, target, cost in times:
+            if source in routes_map:
+                routes_map[source].append((target, cost))
+            else:
+                routes_map[source] = [(target, cost)]
+            times_map[source] = float("inf")
+            if target not in times_map:
+                times_map[target] = float("inf")
+
+        if len(times_map) != n:
+            return -1
+
+        que = [k]
+        times_map[k] = 0
+        while que:
+            source = que.pop()
+            if source not in routes_map:
+                continue
+            routes = routes_map[source]
+            for target, cost in routes:
+                new_time = cost + times_map[source]
+
+                if new_time < times_map[target]:
+                    times_map[target] = new_time
+                    que.append(target)
+        return max(times_map.values()) if float("inf") not in times_map.values() else -1
+
+    # min heap dijkstra
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        pass
+        graph = defaultdict(list)
+
+        for source, target, cost in times:
+            graph[source].append((target, cost))
+
+        dist = {i: float("inf") for i in range(1, n + 1)}
+        dist[k] = 0
+
+        heap = [(0, k)]
+
+        while heap:
+            time, node = heapq.heappop(heap)
+
+            if time > dist[node]:
+                continue
+
+            for target, cost in graph[node]:
+                new_time = time + cost
+
+                if new_time < dist[target]:
+                    dist[target] = new_time
+                    heapq.heappush(heap, (new_time, target))
+
+        answer = max(dist.values())
+        return answer if answer != float("inf") else -1
+
+
+times = [[1, 2, 1], [2, 1, 3]]
+n = 2
+k = 2
+
+print(Solution().networkDelayTime(times, n, k))
+assert Solution().networkDelayTime(times, n, k) == 3
 
 
 times = [[2, 1, 1], [2, 3, 1], [3, 4, 1]]
