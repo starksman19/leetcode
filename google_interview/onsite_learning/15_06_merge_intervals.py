2e2d0f825b637aebd3f394a7d2e5a4cf940fe760
Bartek <bartekg42@gmail.com>
2026-06-15 21:08:36 +0200

    15.06 2 tasks

--- google_interview/onsite_learning/15_06_merge_intervals.py
+++ google_interview/onsite_learning/15_06_merge_intervals.py
@@ -1,13 +1,32 @@
 # LeetCode: https://leetcode.com/problems/merge-intervals/
-# Given an array of intervals, merge all overlapping intervals and return the non-overlapping result.
+# Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
+# and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
 from typing import List
 
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        pass
+        intervals.sort(key=lambda x: x[0])
+        ret = []
+        for start, stop in intervals:
+            if not ret:
+                ret.append([start, stop])
+                continue
 
+            prev = ret[-1]
+            if start <= prev[1] < stop:
+                prev[1] = stop
+            else:
+                if prev[1] < stop:
+                    ret.append([start, stop])
+        return ret
+
+
+intervals = [[1, 4], [2, 3]]
+
+print(Solution().merge(intervals))
+assert Solution().merge(intervals) == [[1, 4]]
 
 intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
 
--- google_interview/onsite_learning/15_06_spiral_matrix.py
+++ google_interview/onsite_learning/15_06_spiral_matrix.py
@@ -6,7 +6,32 @@ from typing import List
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        pass
+        if not matrix or not matrix[0]:
+            return []
+        left, right = 0, len(matrix[0]) - 1
+        top, bottom = 0, len(matrix) - 1
+        ret = []
+
+        while left <= right and top <= bottom:
+            for i in range(left, right + 1):
+                ret.append(matrix[top][i])
+            top += 1
+
+            for i in range(top, bottom + 1):
+                ret.append(matrix[i][right])
+            right -= 1
+
+            if top <= bottom:
+                for i in range(right, left - 1, -1):
+                    ret.append(matrix[bottom][i])
+                bottom -= 1
+
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    ret.append(matrix[i][left])
+                left += 1
+
+        return ret
 
 
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
@@ -18,3 +43,8 @@ matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
 
 print(Solution().spiralOrder(matrix))
 assert Solution().spiralOrder(matrix) == [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
+
+matrix = [[1], [2], [3]]
+
+print(Solution().spiralOrder(matrix))
+assert Solution().spiralOrder(matrix) == [1, 2, 3]
