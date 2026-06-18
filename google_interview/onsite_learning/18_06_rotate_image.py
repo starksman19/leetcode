e67d1e9d34990546c63635031f6ba3cf07684f98
Bartek <bartekg42@gmail.com>
2026-06-18 22:25:39 +0200

    18.06 tasks

--- google_interview/onsite_learning/18_06_rotate_image.py
+++ google_interview/onsite_learning/18_06_rotate_image.py
@@ -6,7 +6,37 @@ from typing import List
 
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        pass
+        n = len(matrix) - 1
+        visited = set()
+        for i in range(n):
+            for j in range(n):
+                if (i, j) not in visited:
+                    row, col = i, j
+                    start = (i, j)
+                    val = matrix[row][col]
+
+                    while True:
+                        new_row = col
+                        new_col = n - row
+                        next_val = matrix[new_row][new_col]
+                        matrix[new_row][new_col] = val
+
+                        row = new_row
+                        col = new_col
+                        val = next_val
+                        visited.add((row, col))
+                        if (row, col) == start:
+                            break
+
+    def rotate2(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for row in matrix:
+            row.reverse()
 
 
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
@@ -20,3 +50,15 @@ matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
 Solution().rotate(matrix)
 print(matrix)
 assert matrix == [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]
+
+
+# r   c      r  c
+# (0, 0) -> (0, 2) ->r-
+# (0, 1) -> (1, 2)
+# (0, 2) -> (2, 2)
+#
+# (2, 1) -> (1, 0)
+#
+# 0,0 -> (0, 2)
+# 1,0 -> (0, 1)
+# 2,0 -> (0, 0)
--- google_interview/onsite_learning/18_06_set_matrix_zeroes.py
+++ google_interview/onsite_learning/18_06_set_matrix_zeroes.py
@@ -6,7 +6,23 @@ from typing import List
 
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        pass
+        m = len(matrix)
+        n = len(matrix[0])
+
+        zero_rows = set()
+        zero_cols = set()
+        for row in range(m):
+            for col in range(n):
+                if matrix[row][col] == 0:
+                    zero_rows.add(row)
+                    zero_cols.add(col)
+
+        for item in zero_rows:
+            for i in range(n):
+                matrix[item][i] = 0
+        for item in zero_cols:
+            for i in range(m):
+                matrix[i][item] = 0
 
 
 matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
