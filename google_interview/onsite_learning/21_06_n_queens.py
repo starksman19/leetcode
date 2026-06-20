7f3d3a91af6857e9857d7497d11bd57d9ac6db33
Bartek <bartekg42@gmail.com>
2026-06-20 23:20:24 +0200

    21.06 second task

--- google_interview/onsite_learning/21_06_n_queens.py
+++ google_interview/onsite_learning/21_06_n_queens.py
@@ -1,6 +1,10 @@
 # LeetCode: https://leetcode.com/problems/n-queens/
-# Given an integer n, return all distinct ways to place n queens on an n x n chessboard
-# so that no two queens attack each other.
+# The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+#
+# Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+#
+# Each solution contains a distinct board configuration of the n-queens' placement,
+# where 'Q' and '.' both indicate a queen and an empty space, respectively.
 
 from typing import List
 
--- google_interview/onsite_learning/21_06_word_search_ii.py
+++ google_interview/onsite_learning/21_06_word_search_ii.py
@@ -1,13 +1,68 @@
 # LeetCode: https://leetcode.com/problems/word-search-ii/
-# Given a board of characters and a list of words, return all words that can be formed in the board.
-# Each word must use adjacent horizontal or vertical cells, and a cell may not be reused within one word.
+# Given an m x n board of characters and a list of strings words, return all words on the board.
+#
+# Each word must be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring.
+# The same letter cell may not be used more than once in a word.
 
 from typing import List
 
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = None
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str):
+        node = self.root
+        for s in word:
+            if s not in node.children:
+                node.children[s] = TrieNode()
+            node = node.children[s]
+        node.word = word
+
+
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        pass
+        if not board:
+            return []
+        rows = len(board)
+        cols = len(board[0])
+        ret = []
+        trie = Trie()
+        for word in words:
+            trie.insert(word)
+
+        def dfs(i: int, j: int, trie_node: TrieNode):
+            if i >= rows or i < 0:
+                return
+            if j >= cols or j < 0:
+                return
+
+            char = board[i][j]
+            if char == "." or char not in trie_node.children:
+                return
+
+            next_node = trie_node.children[char]
+            if next_node.word:
+                ret.append(next_node.word)
+                next_node.word = None
+
+            board[i][j] = "."
+            dfs(i + 1, j, trie_node.children[char])
+            dfs(i - 1, j, trie_node.children[char])
+            dfs(i, j + 1, trie_node.children[char])
+            dfs(i, j - 1, trie_node.children[char])
+            board[i][j] = char
+
+        for r in range(rows):
+            for c in range(cols):
+                dfs(r, c, trie.root)
+        return ret
 
 
 board = [["o", "a", "a", "n"], ["e", "t", "a", "e"], ["i", "h", "k", "r"], ["i", "f", "l", "v"]]
